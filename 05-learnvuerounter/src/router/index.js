5d554d43d0d437b076487715dc4a73f3df221e61
qjc <775320358@qq.com>
2020-05-06 15:01:17 +0800

    守卫导航

--- 05-learnvuerounter/src/router/index.js
+++ 05-learnvuerounter/src/router/index.js
@@ -22,6 +22,9 @@ const routes = [{
   {
     path: '/home',
     component: Home,
+    meta:{
+      title: '首页'
+    },
     children: [{
         path: '',
         redirect: 'news'
@@ -38,15 +41,24 @@ const routes = [{
   },
   {
     path: '/about',
-    component: About
+    component: About,
+    meta:{
+      title: '关于'
+    },
   },
   {
     path: '/user/:userId',
-    component: User
+    component: User,
+    meta:{
+      title: '用户'
+    },
   },
   {
     path: '/profile',
-    component: Profile
+    component: Profile,
+    meta:{
+      title: '档案'
+    },
   }
 ]
 
@@ -55,6 +67,12 @@ const router = new VueRouter({
   routes,
   mode: 'history',
   linkActiveClass: 'active'
+})
+
+router.beforeEach((to,from,next) => {
+  //从from跳转到to
+  document.title = to.matched[0].meta.title  
+  next()
 })
 
 // 3.将router对象传入到Vue实例
