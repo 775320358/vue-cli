e7874cda42f1e34833d97bcd7d1d46c4abce4ce0
qjc <775320358@qq.com>
2020-05-07 10:45:51 +0800

    封装tabbar

--- 06-tabbar/src/App.vue
+++ 06-tabbar/src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <div id="app">
-    <div id="tab-bar">
-      <div class="tab-bar-item">首页</div>
-      <div class="tab-bar-item">分类</div>
-      <div class="tab-bar-item">购物车</div>
-      <div class="tab-bar-item">我的</div>
-    </div>
+    <router-view></router-view>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor="deepPink">
+        <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="deepPink">
+        <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/category_active.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="deepPink">
+        <img slot="item-icon" src="./assets/img/tabbar/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/shopcart_active.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="deepPink">
+        <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/profile_active.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
+import TabBar from './components/tabbar/TabBar'
+import TabBarItem from './components/tabbar/TabBarItem'
 export default {
   name: 'App',
-  components: {}
+  components: {
+    TabBar,
+    TabBarItem
+  }
 }
 </script>
 
@@ -23,9 +45,9 @@ export default {
   background-color: #f6f6f6;
   position: fixed;
   left: 0;
-  right:0;
+  right: 0;
   bottom: 0;
-  box-shadow: 0 -1px 1px rgba(100,100,100,.1);
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.1);
 }
 .tab-bar-item {
   flex: 1;
